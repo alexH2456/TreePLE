2fb29c942a75d5b29c251cfe7d176b1b5b7654f8
Alexander Harris <alexander.harris@mail.mcgill.ca>
2018-02-25 12:59:19 -0500

    Resolved gradle dependencies

---
+++ TreePLE-Spring/src/main/java/ca/mcgill/ecse321/treeple/controller/TreepleRestController.java
@@ -0,0 +1,23 @@
+package ca.mcgill.ecse321.treeple.controller;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TreepleRestController {
+
+}
