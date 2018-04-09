c3235bfa1c25a2f52fbb4cb9bd2c0879c82d6ed7
Abbas Yadollahi <abbas.yadollahi@mail.mcgill.ca>
2018-04-09 18:07:24 -0400

    Test Jenkins

--- TreePLE-Spring/deployment/deploy.sh
+++ TreePLE-Spring/deployment/deploy.sh
@@ -3,6 +3,6 @@
 systemctl stop tomcat
 rm -rf /opt/tomcat/webapps/ROOT
 rm -f /opt/tomcat/webapps/ROOT.war
-cp $WORKSPACE/build/libs/treeple*.war /opt/tomcat/webapps/ROOT.war
+cp $WORKSPACE/TreePLE-Spring/build/libs/treeple*.war /opt/tomcat/webapps/ROOT.war
 systemctl start tomcat
 
--- TreePLE-Web/deployment/deploy.sh
+++ TreePLE-Web/deployment/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 systemctl stop httpd
-npm --prefix $WORKSPACE/TreePLE-Web install
-npm run --prefix $WORKSPACE/TreePLE-Web build
+#npm --prefix $WORKSPACE/TreePLE-Web install
+npm --prefix /run/jenkins/workspace/TreePLE2/TreePLE-Web install
+#npm run --prefix $WORKSPACE/TreePLE-Web build
+npm run --prefix /run/jenkins/workspace/TreePLE2/TreePLE-Web build
 cp -f $WORKSPACE/TreePLE-Web/dist/bundle.js /var/www/html/
 cp -f $WORKSPACE/TreePLE-Web/index.html /var/www/html/
 systemctl start httpd
