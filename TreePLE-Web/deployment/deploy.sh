6586b86f267c046feb7c04cef7549d6f0bdc5b12
Abbas Yadollahi <abbas.yadollahi@mail.mcgill.ca>
2018-04-09 20:16:32 -0400

    Kill me please

--- TreePLE-Web/deployment/deploy.sh
+++ TreePLE-Web/deployment/deploy.sh
@@ -5,7 +5,7 @@ systemctl stop httpd
 #npm run --prefix $WORKSPACE/TreePLE-Web build
 #cp -f $WORKSPACE/TreePLE-Web/dist/bundle.js /var/www/html/
 #cp -f $WORKSPACE/TreePLE-Web/index.html /var/www/html/
-npm --prefix $1/TreePLE-Web install
+npm --prefix $1/TreePLE-Web install --unsafe-perm
 npm run --prefix $1/TreePLE-Web build
 cp -f $1/TreePLE-Web/dist/bundle.js /var/www/html/
 cp -f $1/TreePLE-Web/index.html /var/www/html/
